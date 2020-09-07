ef6c5f2574849515da898e8632b23a83d9e64923
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-09-06 22:04:43 -0400

    design_implementation_started

--- HelloReactNative/App.js
+++ HelloReactNative/App.js
@@ -38,23 +38,124 @@ const App: () => React$Node = () => {
   
   return (
     <>
-      <SafeAreaView>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.mapToolbar}>
+        
+        </View>
         <ScrollView
-          contentInsetAdjustmentBehavior="automatic">
-          <View>
-            <Text>
-              Overthrow capitalism!
-            </Text>
-            
-            <TouchableOpacity onPress={findCoordinates}>
-              <Text>Find My Coords?</Text>
-              <Text>Location: {location}</Text>
-            </TouchableOpacity>
+          style={styles.pageButtons}
+          contentInsetAdjustmentBehavior="automatic"
+          horizontal={true}
+          scrollEnabled={true}
+        >
+          <View style={styles.pageButton}>
+            <View style={styles.pageButtonText}>
+              <Text style={styles.text}>
+                COVID Status
+              </Text>
+              <Text style={styles.text}>
+                Check-In
+              </Text>
+            </View>
+          </View>
+          
+          <View style={styles.pageButton}>
+            <View style={styles.pageButtonText}>
+              <Text style={styles.text}>
+                Location
+              </Text>
+              <Text style={styles.text}>
+                Logging
+              </Text>
+            </View>
+          </View>
+          
+          <View style={styles.pageButton}>
+            <View style={styles.pageButtonText}>
+              <Text style={styles.text}>
+                Community
+              </Text>
+              <Text style={styles.text}>
+                Circles
+              </Text>
+            </View>
+          </View>
+          
+          <View style={styles.pageButton}>
+            <View style={styles.pageButtonText}>
+              <Text style={styles.text}>
+                Community
+              </Text>
+              <Text style={styles.text}>
+                Support
+              </Text>
+            </View>
+          </View>
+          
+          <View style={styles.pageButton}>
+            <View style={styles.pageButtonText}>
+              <Text style={styles.text}>
+                Community
+              </Text>
+              <Text style={styles.text}>
+                Report
+              </Text>
+            </View>
+          </View>
+          
+          <View style={styles.pageButton}>
+            <View style={styles.pageButtonText}>
+              <Text style={[styles.text, styles.settingsText]}>
+                Settings
+              </Text>
+            </View>
           </View>
         </ScrollView>
       </SafeAreaView>
     </>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'flex-end'
+  },
+  
+  mapToolbar: {
+    //backgroundColor: 'green',
+    height: 110,
+  },
+  
+  pageButtons: {
+    paddingLeft: 20,
+    maxHeight: 195
+  },
+  
+  pageButton: {
+    width: 221,
+    height: 132,
+    borderRadius: 6,
+    backgroundColor: '#4F4F4F',
+    marginRight: 8
+  },
+  
+  pageButtonText: {
+    marginTop: 50,
+    marginLeft: 25
+  },
+  
+  text: {
+    fontSize: 24,
+    color: 'white',
+    lineHeight: 24,
+    fontWeight: 'bold'
+  },
+  
+  settingsText: {
+    marginTop: 11
+  }
+});
 
 export default App;
